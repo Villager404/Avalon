57653fad4dcf4e5d0a1e49b873f8f8b6fdb05e09
ilien <sks6261@naver.com>
2024-08-04 17:32:08 +0900

    Binary Search 구현

---
+++ Avalon/BinarySearch.cs
@@ -0,0 +1,31 @@
+﻿namespace Avalon
+{
+    internal class BinarySearch
+    {
+        public static int Search(int[] array, int target)
+        {
+            int left = 0;
+            int right = array.Length - 1;
+
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+
+                // Check if target is present at mid
+                if (array[mid] == target)
+                    return mid;
+
+                // If target greater, ignore left half
+                if (array[mid] < target)
+                    left = mid + 1;
+
+                // If target is smaller, ignore right half
+                else
+                    right = mid - 1;
+            }
+
+            // If we reach here, the element was not present
+            return -1;
+        }
+    }
+}
