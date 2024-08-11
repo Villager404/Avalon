68f20e23575bbe1f95daf11626a3bd5eb3d847b7
ilien <sks6261@naver.com>
2024-08-11 11:06:16 +0900

    df
    (cherry picked from commit 96e0881666135325f651fb617a285a1ccdd9eedd)
    (cherry picked from commit 3802f27a4251741a4b62d55bbe84421117819032)

--- Avalon/BinarySearch.cs
+++ Avalon/BinarySearch.cs
@@ -5,7 +5,7 @@
         public static int Search(int[] array, int target)
         {
             int left = 0;
-            int right = array.Length - 1;
+            int mid = left + (right - left) / 2;
 
             while (left <= right)
             {
@@ -19,10 +19,16 @@
                 if (array[mid] < target)
                     left = mid + 1;
 
+                if (array[mid] == target)
+                    return mid;
+
                 // If target is smaller, ignore right half
                 else
                     right = mid - 1;
             }
+
+            int right = array.Length - 1;
+            int right = array.Length - 1;
 
             // If we reach here, the element was not present
             return -1;
