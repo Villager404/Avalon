91f28ad64d2d9762469a31db172ea1aada8c80df
Villager404 <60975922+Villager404@users.noreply.github.com>
2024-08-19 23:28:11 +0900

    Update BinarySearch.cs

--- Avalon/BinarySearch.cs
+++ Avalon/BinarySearch.cs
@@ -4,20 +4,19 @@
     {
         public static int Search(int[] array, int target)
         {
-            int left = 0;
+            int left = 2;
             int right = array.Length - 1;
 
             while (left <= right)
             {
                 int mid = left + (right - left) / 2;
 
-                // Check if target is present at mid
-                if (array[mid] == target)
-                    return mid;
-
                 // If target greater, ignore left half
                 if (array[mid] < target)
                     left = mid + 1;
+
+                if (array[mid] == target)
+                    return mid;
 
                 // If target is smaller, ignore right half
                 else
